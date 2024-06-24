c8e0e738fce77d669cb3aeb04d54725039fb84f5
Gulshair <gulshairawan635@gmail.com>
2024-06-24 18:04:15 +0500

    Navbar Changes

--- src/Compunents/Navbar.jsx
+++ src/Compunents/Navbar.jsx
@@ -17,16 +17,12 @@ const Navbar = () => {
     setIsMobileMenuOpen((prevState) => !prevState);
   };
 
-
-
-
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleModal = () => {
     console.log("Toggle modal");
     setIsModalOpen(!isModalOpen);
   };
-
 
   const [isModalSignUp, setIsModalSignUp] = useState(false);
 
@@ -39,22 +35,15 @@ const Navbar = () => {
     <>
       <nav className="  bg-transparent">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-
-          <img
-            src="/logo.png"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
-
+          <img src="/logo.png" className="h-8" alt="Flowbite Logo" />
 
           <button
             onClick={toggleMobileMenu}
             data-collapse-toggle="navbar-default"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
             aria-controls="navbar-default"
             aria-expanded="false"
-
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -63,7 +52,6 @@ const Navbar = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 17 14"
-
             >
               <path
                 stroke="currentColor"
@@ -74,14 +62,18 @@ const Navbar = () => {
               />
             </svg>
           </button>
-          <div className={`${isMobileMenuOpen ? "block" : "hidden"
-            } w-full md:block md:w-auto`} id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border bg-black md:bg-transparent border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <div
+            className={`${
+              isMobileMenuOpen ? "block" : "hidden"
+            } w-full md:block md:w-auto`}
+            id="navbar-default"
+          >
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border bg-black md:bg-transparent border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
               <li>
                 <NavLink
-                  to='/'
-                  className="block py-2 px-3 text-white  rounded md:bg-transparent  md:p-0 dark:text-white md:dark:text-blue-500"
-                // aria-current="page"
+                  to="/"
+                  className="block py-2 px-3 text-white  rounded md:bg-transparent  md:p-0 "
+                  // aria-current="page"
                 >
                   Home
                 </NavLink>
@@ -89,7 +81,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#Whoarewe"
-                  className="block py-2 px-3 text-white rounded  md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 text-white rounded  md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 "
                 >
                   Who are we
                 </a>
@@ -97,7 +89,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#Services"
-                  className="block py-2 px-3 text-white rounded md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 text-white rounded md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500"
                 >
                   Services
                 </a>
@@ -105,7 +97,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#Blogs"
-                  className="block py-2 px-3 text-white rounded  md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 text-white rounded  md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500"
                 >
                   Latest News
                 </a>
@@ -113,14 +105,13 @@ const Navbar = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-white rounded  md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 text-white rounded  md:hover:bg-transparent md:border-0  md:p-0 "
                   onClick={SignUpModal}
                 >
                   Sign Up
                 </a>
               </li>
               <li>
-
                 <button
                   onClick={toggleModal}
                   className="px-[20px] text-theme  py-1 border border-secColor rounded-full bg-secColor  duration-200 "
@@ -132,9 +123,6 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-
-
-
 
       {isModalOpen && <Login toggle={toggleModal} />}
       {isModalSignUp && <SignUp toggle={SignUpModal} />}
